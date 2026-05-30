711d0bbfdabe2e58e88190292a163021289b18cd
SeeTwoo <waltibee@gmail.com>
2026-05-30 11:55:01 +0200

    copy from what was on the vm. just says hello for now

---
+++ driver.c
@@ -0,0 +1,47 @@
+#include <linux/module.h>
+#include <linux/printk.h>
+#include <linux/init.h>
+#include <linux/interrupt.h>
+
+#ifndef DA_KEYBOARD_IRQ
+# define DA_KEYBOARD_IRQ 1
+#endif
+
+#ifndef MY_DEV_NAME
+# define MY_DEV_NAME "shit"
+#endif
+
+static irqreturn_t	my_handler(int irq, void *dev_id)
+{
+	pr_info("Interrupt occured\n");
+	return IRQ_HANDLED;
+}
+
+static int __init hello_1_init(void)
+{
+	pr_info("hello, world 1.\n");
+
+	int	result;
+
+	result = request_irq(DA_KEYBOARD_IRQ,
+			my_handler,
+			IRQF_SHARED,
+			MY_DEV_NAME,
+			(void *)(my_handler));
+	if (result) {
+		pr_err("Failed to reserve IRQ");
+		return result;
+	}
+	return 0;
+}
+
+static void __exit hello_1_exit(void)
+{
+	free_irq(DA_KEYBOARD_IRQ, (void *)(my_handler));
+	pr_info("goodbye, world 1.\n");
+}
+
+module_init(hello_1_init);
+module_exit(hello_1_exit);
+
+MODULE_LICENSE("GPL");
