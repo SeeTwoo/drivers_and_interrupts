98bf1abae624c114e5f3f13d6bb62647f4224701
SeeTwoo <waltibee@gmail.com>
2026-06-02 04:22:55 +0200

    trying something with keyboard notifier

--- driver.c
+++ driver.c
@@ -1,7 +1,9 @@
-#include <linux/module.h>
-#include <linux/printk.h>
 #include <linux/init.h>
 #include <linux/interrupt.h>
+#include <linux/keyboard.h>
+#include <linux/module.h>
+#include <linux/notifier.h>
+#include <linux/printk.h>
 
 #ifndef DA_KEYBOARD_IRQ
 # define DA_KEYBOARD_IRQ 1
@@ -10,6 +12,26 @@
 #ifndef MY_DEV_NAME
 # define MY_DEV_NAME "shit"
 #endif
+
+//part about keyboard notifiers
+
+static int	keyboard_event(struct notifier_block *nb, unsigned long action, void *data)
+{
+	struct keyboard_notifier_param	*param = data;
+
+	if (action != KBD_KEYCODE)
+		return NOTIFY_OK;
+
+	pr_info("keycode=%u %s\n", param->value, param->down ? "pressed" : "released");
+	return NOTIFY_OK;
+}
+
+static struct notifier_block	keyboard_nb = {
+	.notifier_call = keyboard_event,
+};
+
+
+//part about interrupts
 
 struct s_device {
 	int		irq;
@@ -20,7 +42,7 @@ static struct s_device	my_dev;
 
 static irqreturn_t	my_handler(int irq, void *dev_id)
 {
-	pr_info("Interrupt occured\n");
+	//pr_info("Interrupt occured\n");
 	return IRQ_HANDLED;
 }
 
@@ -39,12 +61,14 @@ static int __init hello_1_init(void)
 		pr_err("Failed to reserve IRQ");
 		return result;
 	}
+	register_keyboard_notifier(&keyboard_nb);
 	return 0;
 }
 
 static void __exit hello_1_exit(void)
 {
 	free_irq(DA_KEYBOARD_IRQ, (void *)(&my_dev));
+	unregister_keyboard_notifier(&keyboard_nb);
 	pr_info("goodbye, world 1.\n");
 }
 
