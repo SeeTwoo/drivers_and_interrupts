20863af83db2c974065d01138d12d161fc915c67
SeeTwoo <waltibee@gmail.com>
2026-06-09 02:07:08 +0200

    adding file lock

--- mandatory/main.c
+++ mandatory/main.c
@@ -19,6 +19,21 @@ extern const struct s_key keys_table[];
 static LIST_HEAD(keystroke_list);
 static DEFINE_SPINLOCK(log_spinlock);
 
+static atomic_t	device_opened = ATOMIC_INIT(0);
+
+static int	my_open(struct inode *inode, struct file *file)
+{
+	if (atomic_cmpxchg(&device_opened, 0, 1))
+		return -EBUSY;
+	return 0;
+}
+
+static int	my_release(struct inode *inode, struct file *file)
+{
+	atomic_set(&device_opened, 0);
+	return 0;
+}
+
 static ssize_t	my_read(struct file *file, char __user *out, size_t len, loff_t *off)
 {
 	struct s_keystroke	*entry;
@@ -59,6 +74,8 @@ out:
 
 static const struct file_operations fops = {
 	.owner = THIS_MODULE,
+	.open = my_open,
+	.release = my_release,
 	.read = my_read,
 };
 
