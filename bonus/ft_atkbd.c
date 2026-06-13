d4938baed1e7d7c1ff666a674cce41ca3d3b5ab6
waltibee <waltibee@gmail.com>
2026-06-13 17:10:45 +0200

    compiling errors

--- bonus/ft_atkbd.c
+++ bonus/ft_atkbd.c
@@ -57,9 +57,9 @@ static int __init hello_1_init(void)
 		ret = -ENOMEM;
 		goto err_free_irq;
 	}
-	set_bit(EV_KEY; ft_keyboard->evbit);
+	set_bit(EV_KEY, ft_keyboard->evbit);
 	for (int i = 0; i <= MAX_KEY; i++)
-		setbit(i, ft_keyboard->keybit);
+		set_bit(i, ft_keyboard->keybit);
 	ret = input_register_device(ft_keyboard);
 	if (ret) {
 		printk(KERN_ERR "ft_atkbd.c: Failed to register device\n");
