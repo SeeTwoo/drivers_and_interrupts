e2fffb136ebe09fc926ed53a9172bacf28d72fb7
waltibee <waltibee@gmail.com>
2026-06-13 17:51:09 +0200

    hello

--- update.sh
+++
@@ -1,3 +0,0 @@
-cp -v bonus/ft_atkbd.c /sources/linux-6.18.10/drivers/input/keyboard && \
-make -j$(nproc) -C /sources/linux-6.18.10 && \
-cp -v /sources/linux-6.18.10/arch/x86/boot/bzImage /boot/vmlinuz-6.18.10-ft_atkbd
